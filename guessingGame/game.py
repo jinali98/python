9abde5722236b71fc3df62a342092978f6fe244d
jinali98 <jinalipabasara@gmail.com>
2021-12-08 09:30:46 +0530

    first commit

---
+++ guessingGame/game.py
@@ -0,0 +1,13 @@
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+
+while guess_count < guess_limit:
+    user_input = int(input('Guess : '))
+    guess_count += 1
+    if user_input == secret_number:
+        print('you won')
+        break
+else:
+    print('you lost')
+
